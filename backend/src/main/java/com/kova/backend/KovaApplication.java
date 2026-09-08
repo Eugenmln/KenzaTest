140923d54b60434ae4b7ad7282d6192e3a3a1807
Eugenmln <81273576+Eugenmln@users.noreply.github.com>
2026-09-08 20:05:41 -0300

    feat(backend): add application entrypoint

---
+++ backend/src/main/java/com/kova/backend/KovaApplication.java
@@ -0,0 +1,11 @@
+package com.kova.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KovaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KovaApplication.class, args);
+    }
+}
